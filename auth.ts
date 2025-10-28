c05681b96d35d49e8d69ce1d499ae3779c2fa34e
Stephane Mfuni <stephuni35@gmail.com>
2025-10-28 19:49:26 +0100

    Refactor authentication logic and remove unused types

--- auth.ts
+++ auth.ts
@@ -2,18 +2,16 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import Google from "next-auth/providers/google";
 import { AUTH_SECRET, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from "./lib/env";
 import NextAuth from "next-auth";
-import { roleUserType } from "./types/auth";
+
 import { prisma } from "@/lib/prisma";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
   pages: {
-    // signIn: "/connexion",
     verifyRequest: "/verif-email-sent",
   },
   secret: AUTH_SECRET!,
   trustHost: true,
-
   providers: [
     Google({
       clientId: GOOGLE_CLIENT_ID!,
@@ -36,24 +34,34 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
         const tokenId = token.sub;
         if (tokenId) {
           const user = await prisma.user.findUnique({
-            where: {
-              id: tokenId,
+            where: { id: tokenId },
+            select: {
+              id: true,
+              email: true,
+              name: true,
+              emailVerified: true,
+              image: true,
+              role: true,
+              tel: true,
+              address: true,
+              createdAt: true,
+              updatedAt: true,
+              isBanned: true,
             },
-            omit: { password: true },
           });
           if (user) {
             session.user = {
               id: user.id,
+              name: user.name,
               email: user.email ? user.email : "",
-              name: user.name ? user.name : "",
               emailVerified: user.emailVerified,
               image: user.image,
-              role: user.role as roleUserType,
-              tel: user.tel || "",
-              address: user.address || "",
-              createdAt: user.createdAt,
-              updatedAt: user.updatedAt,
-              isBanned: user.isBanned,
+              // role: user.role as roleUserType,
+              // tel: user.tel || "",
+              // address: user.address || "",
+              // createdAt: user.createdAt,
+              // updatedAt: user.updatedAt,
+              // isBanned: user.isBanned,
             };
           }
         }
