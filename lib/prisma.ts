d1356434536997d2eb34129970d4a01848449fdd
Stephane Mfuni <stephuni35@gmail.com>
2025-12-14 00:28:03 +0100

    fix prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -5,6 +5,7 @@ import { Pool } from "pg"; // 💡 Nécessite l'installation de la dépendance '
 import { PrismaClient } from "./generated/prisma/client";
 
 declare global {
+  // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
 }
 
